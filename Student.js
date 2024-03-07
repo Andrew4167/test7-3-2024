9ee2d66a8eaaa68bd6d3764be1d69d503d296958
Andrew4167 <150346251+Andrew4167@users.noreply.github.com>
2024-03-07 14:35:28 +0100

    Add files via upload

---
+++ Student.js
@@ -0,0 +1,10 @@
+class Student { // 4.
+
+    constructor(jmeno, prijmeni, vek, nazev) {
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.vek = vek;
+        this.nazev = nazev;
+    }
+    
+}
