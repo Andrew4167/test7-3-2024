a77809d9442d6012a7e0b57e56edc72f49a8fd25
Andrew4167 <150346251+Andrew4167@users.noreply.github.com>
2024-03-07 14:03:55 +0100

    Add files via upload

---
+++ Clazz.js
@@ -0,0 +1,11 @@
+class Clazz {
+
+    constructor(nazev) {
+        this.nazev = nazev;
+        this.Student = [];
+    }
+    
+    vypisInfoOFirme() {
+        document.write("Firma " + this.nazev + " sídlí v " + this.sidlo);
+    }
+}
